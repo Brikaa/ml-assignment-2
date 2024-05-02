5b8716ab957efc673b4c4cce6cb7afa55afd9c14
Omar Brikaa <brikaaomar@gmail.com>
2024-05-02 15:41:34 +0300

    Accuracy in percentage in decision trees

--- problem1.py
+++ problem1.py
@@ -24,7 +24,7 @@ def run_experiment(training_size, verbose=False):
 
         tree = DecisionTreeClassifier().fit(training_features, training_targets)
         size = tree.tree_.node_count
-        accuracy = tree.score(test_features, test_targets)
+        accuracy = tree.score(test_features, test_targets) * 100
         sum_accuracy += accuracy
         min_accuracy = min(min_accuracy, accuracy)
         max_accuracy = max(max_accuracy, accuracy)
@@ -32,7 +32,7 @@ def run_experiment(training_size, verbose=False):
         min_size = min(min_size, size)
         max_size = max(max_size, size)
         if verbose:
-            print(f"{i + 1}. Tree size: {size}, accuracy: {accuracy}")
+            print(f"{i + 1}. Tree size: {size}, accuracy: {accuracy}%")
     return (
         sum_accuracy / no_trials,
         min_accuracy,
@@ -47,19 +47,19 @@ print("Training dataset: 25%")
 run_experiment(0.25, True)
 print()
 
-train_sizes = [0.3, 0.4, 0.5, 0.6, 0.7]
+train_sizes = [30, 40, 50, 60, 70]
 mean_accuracies = []
 mean_sizes = []
 for train_size in train_sizes:
-    print(f"Training dataset: {train_size*100}%")
+    print(f"Training dataset: {train_size}%")
     mean_accuracy, min_accuracy, max_accuracy, mean_size, min_size, max_size = (
-        run_experiment(train_size)
+        run_experiment(train_size / 100)
     )
     mean_accuracies.append(mean_accuracy)
     mean_sizes.append(mean_size)
-    print(f"Mean accuracy: {mean_accuracy}")
-    print(f"Maximum accuracy: {max_accuracy}")
-    print(f"Minimum accuracy: {min_accuracy}")
+    print(f"Mean accuracy: {mean_accuracy}%")
+    print(f"Maximum accuracy: {max_accuracy}%")
+    print(f"Minimum accuracy: {min_accuracy}%")
     print(f"Mean size: {mean_size}")
     print(f"Maximum size: {max_size}")
     print(f"Minimum size: {min_size}")
